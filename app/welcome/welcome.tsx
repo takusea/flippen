7fd71d76dfe858153b952d42ca81acb860d13153
Takusea <takusea826@gmail.com>
2025-05-26 20:01:25 +0900

    IconButtonにラベルを追加

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -17,24 +17,29 @@ import IconButton from "~/base/IconButton";
 export function Welcome() {
 	return (
 		<main className="relative w-full h-full">
+			<canvas />
 			<div className="absolute bottom-8 w-fit left-0 right-0 m-auto flex gap-2">
 				<div className="flex p-1 border border-gray-200 rounded-lg shadow-md gap-1">
-					<IconButton icon={IconArrowBackUp} size="small" />
-					<IconButton icon={IconArrowForwardUp} size="small" />
-					<IconButton icon={IconCopy} size="small" />
-					<IconButton icon={IconClipboard} size="small" />
+					<IconButton label="Undo" icon={IconArrowBackUp} size="small" />
+					<IconButton label="Redo" icon={IconArrowForwardUp} size="small" />
+					<IconButton label="Copy" icon={IconCopy} size="small" />
+					<IconButton label="Paste" icon={IconClipboard} size="small" />
 				</div>
 				<div className="flex p-1 border border-gray-200 rounded-lg shadow-md gap-1">
-					<IconButton icon={IconPlayerSkipBack} size="small" />
-					<IconButton icon={IconPlayerPlay} size="small" />
-					<IconButton icon={IconPlayerSkipForward} size="small" />
+					<IconButton label="Rewind" icon={IconPlayerSkipBack} size="small" />
+					<IconButton label="Play" icon={IconPlayerPlay} size="small" />
+					<IconButton
+						label="Forward"
+						icon={IconPlayerSkipForward}
+						size="small"
+					/>
 				</div>
 				<div className="flex p-1 border border-gray-200 rounded-lg shadow-md gap-1">
-					<IconButton icon={IconArrowsMove} size="small" />
-					<IconButton icon={IconPencil} size="small" />
-					<IconButton icon={IconEraser} size="small" />
-					<IconButton icon={IconBucketDroplet} size="small" />
-					<IconButton icon={IconLasso} size="small" />
+					<IconButton label="Move" icon={IconArrowsMove} size="small" />
+					<IconButton label="Pen" icon={IconPencil} size="small" />
+					<IconButton label="Eraser" icon={IconEraser} size="small" />
+					<IconButton label="Fill" icon={IconBucketDroplet} size="small" />
+					<IconButton label="Select" icon={IconLasso} size="small" />
 				</div>
 			</div>
 			<div className="absolute h-full right-0 border-l border-gray-200">
