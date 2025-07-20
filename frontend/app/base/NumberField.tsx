7537609e390f9b11b538528b1c41142a1266521c
Takusea <takusea826@gmail.com>
2025-07-20 15:08:28 +0900

    fix: NumberFieldの挙動を修正

--- frontend/app/base/NumberField.tsx
+++ frontend/app/base/NumberField.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from "react";
+import type React from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = Omit<
 	React.ComponentProps<"input">,
@@ -10,20 +11,26 @@ type Props = Omit<
 	onValueChange?: (value: number) => void;
 };
 
-const TextField: React.FC<Props> = (props) => {
+const NumberField: React.FC<Props> = (props) => {
 	const input = useRef<HTMLInputElement>(null);
 
-	const [isPointerDown, setIsPointerDown] = useState(false);
-	const [isMoved, setIsMoved] = useState(false);
+	const [isMoved, setIsMoved] = useState<boolean>(false);
+	const [innerValue, setInnerValue] = useState<string>(
+		props.value?.toString() ?? "",
+	);
 
-	const cursor = isPointerDown ? "cursor-ew-resize" : "cursor-text";
+	useEffect(() => {
+		setInnerValue(props.value?.toString() ?? "");
+	}, [props.value]);
+
+	const cursor = isMoved ? "cursor-ew-resize" : "cursor-text";
 
 	const handlePointerMove = (event: React.PointerEvent) => {
 		if (!(event.buttons & 1) || props.value == null) return;
 
 		event.currentTarget.setPointerCapture(event.pointerId);
 
-		if (event.movementX > 0) {
+		if (Math.abs(event.movementX) > 0) {
 			setIsMoved(true);
 		}
 
@@ -35,13 +42,7 @@ const TextField: React.FC<Props> = (props) => {
 		);
 	};
 
-	const handlePointerDown = () => {
-		setIsPointerDown(true);
-	};
-
 	const handlePointerUp = () => {
-		setIsPointerDown(false);
-
 		if (!isMoved) {
 			if (input.current == null) {
 				throw new Error("input.current is null");
@@ -56,22 +57,56 @@ const TextField: React.FC<Props> = (props) => {
 		event.stopPropagation();
 	};
 
+	const handleChange = (event: React.ChangeEvent) => {
+		if (!(event.currentTarget instanceof HTMLInputElement)) {
+			throw new Error("event.currentTarget is not instanceof HTMLInputElement");
+		}
+
+		setInnerValue(event.currentTarget.value);
+	};
+
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		if (event.key === "Enter") {
+			if (!(event.currentTarget instanceof HTMLInputElement)) {
+				throw new Error(
+					"event.currentTarget is not instanceof HTMLInputElement",
+				);
+			}
+
+			if (Number.isNaN(event.currentTarget.value)) {
+				throw new Error("event.currentTarget.value is not a number");
+			}
+
+			props.onValueChange?.(
+				Math.max(
+					props.min ?? 0,
+					Math.min(
+						Number.parseFloat(event.currentTarget.value),
+						props.max ?? 100,
+					),
+				),
+			);
+		}
+	};
+
 	return (
 		<div
 			className={`relative h-8 border border-zinc-500/25 bg-zinc-500/25 rounded ${cursor}`}
 			onPointerMove={handlePointerMove}
-			onPointerDown={handlePointerDown}
 			onPointerUp={handlePointerUp}
 		>
 			<input
 				{...props}
 				ref={input}
 				type="number"
+				value={innerValue}
 				className="absolute inset-[-1px] px-2 pointer-events-none focus:pointer-events-auto"
 				onPointerMove={handleInputPointerMove}
+				onChange={handleChange}
+				onKeyDown={handleKeyDown}
 			/>
 		</div>
 	);
 };
 
-export default TextField;
+export default NumberField;
